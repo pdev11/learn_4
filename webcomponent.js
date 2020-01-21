7ba1eec6e25a307032ef77254a97aed7e4b5e326
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2020-01-21 10:26:42 +0100

    initial checkin

---
+++ webcomponent.js
@@ -0,0 +1,97 @@
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+    `;
+
+    customElements.define('com-sap-sample-helloworld2', class HelloWorld1 extends HTMLElement {
+
+
+		constructor() {
+			super(); 
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._tagContainer;
+            this._tagType = "h1";
+            this._tagText = "Hello World";
+		}
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+            this.redraw();
+        }
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        
+        }
+
+        
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default
+        /*
+        onCustomWidgetResize(width, height){
+        
+        }
+        */
+
+        //Getters and Setters
+        get headingType() {
+            return this._tagType;
+        }
+
+        set headingType(value) {
+
+            if (value == "h2"){
+                this._tagType = "h2";
+            } else if (value == "h3"){
+                this._tagType = "h3";
+            } else {
+                this._tagType = "h1";
+            } 
+            this.redraw();
+        }
+
+        get widgetText() {
+            return this._tagType;
+        }
+
+        set widgetText(value) {
+
+
+            this._tagText = value;
+            this.redraw();
+        }
+        // End - Getters and Setters
+
+        redraw(){
+            if (this._tagContainer){
+                this._tagContainer.parentNode.removeChild(this._tagContainer);
+            }
+
+            var shadow = window.getSelection(this._shadowRoot);
+            this._tagContainer = document.createElement(this._tagType);
+            var theText = document.createTextNode(this._tagText);    
+            this._tagContainer.appendChild(theText); 
+            this._shadowRoot.appendChild(this._tagContainer);
+
+        }
+    
+    
+    });
+        
+})();
