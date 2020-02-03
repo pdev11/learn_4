5303b3fb2537b253e769cbf1ae6c5a19a1983020
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2020-02-03 13:57:47 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -12,7 +12,8 @@
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._tagContainer;
             this._tagType = "h1";
-            this._tagText = "Hello World";
+            this._tagText;
+
 		}
 
         //Fired when the widget is added to the html DOM of the page
@@ -63,16 +64,17 @@
         // End - Getters and Setters
 
         redraw(){
-            if (this._tagContainer){
-                this._tagContainer.parentNode.removeChild(this._tagContainer);
+            if (this._tagText != null){
+                if (this._tagContainer){
+                    this._tagContainer.parentNode.removeChild(this._tagContainer);
+                }
+
+                var shadow = window.getSelection(this._shadowRoot);
+                this._tagContainer = document.createElement(this._tagType);
+                var theText = document.createTextNode(this._tagText);    
+                this._tagContainer.appendChild(theText); 
+                this._shadowRoot.appendChild(this._tagContainer);
             }
-
-            var shadow = window.getSelection(this._shadowRoot);
-            this._tagContainer = document.createElement(this._tagType);
-            var theText = document.createTextNode(this._tagText);    
-            this._tagContainer.appendChild(theText); 
-            this._shadowRoot.appendChild(this._tagContainer);
-
         }
     
     
